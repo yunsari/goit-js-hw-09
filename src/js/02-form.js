d4f6a8ca1fed6e458779a38245110e8084f5b556
Yunus <sariyunus@proton.me>
2025-09-15 14:28:24 +0300

    form revised

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,35 +1,42 @@
-const form = document.querySelector(".feedback-form");
-const localStorageKey = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
 const savedData = localStorage.getItem(localStorageKey);
 
 if (savedData) {
   const storageData = JSON.parse(savedData);
-  form.elements.email.value = storageData.email || "";
-  form.elements.message.value = storageData.message || "";
-};
+  form.elements.email.value = storageData.email || '';
+  form.elements.message.value = storageData.message || '';
+}
 
-form.addEventListener("input", () => {
-    const formData = {
-        email: form.elements.email.value.trim(),
-        message: form.elements.message.value.trim()
-    };
+form.addEventListener('input', () => {
+  const formData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
 
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
-form.addEventListener("submit", (event) => {
-    
-    event.preventDefault();
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
-    const formData = {
-        email: form.elements.email.value.trim(),
-        message: form.elements.message.value.trim()
-    };
+  const inputs = form.querySelectorAll('input, textarea');
+  for (let input of inputs) {
+    if (input.value.trim() === '') {
+      alert('Email or Message cant be empty!');
+      return; // Boş alan varsa çıkış yap
+    }
+  }
 
-    console.log("Form Data: ", formData);
+  const formData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
 
-    localStorage.removeItem(localStorageKey);
+  console.log('Form Data: ', formData);
 
-    form.reset();
+  localStorage.removeItem(localStorageKey);
+
+  form.reset();
 });
